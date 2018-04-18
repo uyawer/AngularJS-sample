589e0cd01066bca7490a072e9f522f43ccf67ee5
uyawer <uyawer@gmail.com>
2018-04-18 17:26:59 +0900

    Angular公式サイトのチュートリアル4を手順に合わせて実装
    * https://angular-ja.firebaseapp.com/tutorial/toh-pt2

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Hero} from '../hero';
+import {HEROES} from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -7,11 +8,8 @@ import {Hero} from '../hero';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  /* hero変数を追加 */
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  heroes = HEROES;
+  selectedHero: Hero;
 
   constructor() {
   }
@@ -19,4 +17,11 @@ export class HeroesComponent implements OnInit {
   ngOnInit() {
   }
 
+  /**
+   * heroが選択された時に動作するメソッド
+   * @param {Hero} hero ヒーロー情報
+   */
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 }
