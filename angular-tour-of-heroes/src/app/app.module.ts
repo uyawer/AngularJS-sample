3302fd996e8311fee838e55c1b19bf26714ccab0
uyawer <uyawer@gmail.com>
2018-04-20 13:09:59 +0900

    Angular公式サイトのチュートリアル7を手順に合わせて実装
    * https://angular-ja.firebaseapp.com/tutorial/toh-pt6

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -12,18 +12,33 @@ import {MessageService} from './message.service';
 import {AppRoutingModule} from './app-routing.module';
 import {DashboardComponent} from './dashboard/dashboard.component';
 
+import {HttpClientModule} from '@angular/common/http';
+
+import {HttpClientInMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './in-memory-data.service';
+import {HeroSearchComponent} from './hero-search/hero-search.component';
+
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+
+    // The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+    // and returns simulated server responses.
+    // Remove it when a real server is ready to receive requests.
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, {dataEncapsulation: false}
+    )
+  ],
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
-  ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    AppRoutingModule
+    MessagesComponent,
+    HeroSearchComponent
   ],
   providers: [
     HeroService,
